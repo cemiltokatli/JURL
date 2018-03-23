9f10349e52f92fcb0921b4f42f823bde7a47497e
Cemil Tokatlı <cemiltokatli@yahoo.com>
2018-03-23 15:33:26 +0300

    URLBuildException h class added

---
+++ src/main/java/com/cemiltokatli/jurl/exception/URLBuildException.java
@@ -0,0 +1,7 @@
+package com.cemiltokatli.jurl.exception;
+
+public class URLBuildException extends RuntimeException {
+    public URLBuildException(String message){
+        super(message);
+    }
+}
