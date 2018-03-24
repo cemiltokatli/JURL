16b8087b081528e6dee3da7d27ddee369612d0fc
Cemil Tokatlı <cemiltokatli@yahoo.com>
2018-03-24 17:10:33 +0300

    Exception classes comment updated

--- src/main/java/com/cemiltokatli/jurl/exception/HttpURLParseError.java
+++ src/main/java/com/cemiltokatli/jurl/exception/HttpURLParseError.java
@@ -1,7 +1,7 @@
 package com.cemiltokatli.jurl.exception;
 
 /**
- * Thrown to indicate that the Http(s) url string cannot be built because its building
+ * Thrown to indicate that the Http(s) URL string cannot be built because its building
  * has already been started or it has been created by parsing a string.
  */
 public class HttpURLParseError extends Error {
